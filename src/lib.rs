a943bb9806fed66a7d05b31bfb6b727f589a0d09
Joel Palmer <joelpalmer@gmail.com>
2019-06-20 14:33:17 -0500

    create and impl Worker and refactor ThreadPool to use workers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -12,5 +22,18 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
 
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
